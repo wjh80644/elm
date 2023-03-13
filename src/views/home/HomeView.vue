67a01d852625d04db7f8e1cbca2e504d0841067b
jjbar <806443173@qq.com>
2023-03-13 17:59:32 +0800

    add 主页头部

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <HeadView />
+    <div class="cityTip">
+      <span>当前定位城市：</span>
+      <span>定位不准时，请在城市列表中选择</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeadView from '@/views/home/components/HeadView.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeadView,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import '../../style/mixin';
+.cityTip {
+  margin-top: 2rem;
+  border: 1px solid @gray;
+
+  span:nth-child(1) {
+    .sizeColor(0.5rem,#333);
+  }
+
+  span:nth-child(2) {
+    font-size: 0.2rem;
+  }
+}
+</style>
---
+++ src/views/home/components/HeadView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="head">
+    <span class="home">首页</span>
+    <span class="login">登录|注册</span>
+  </div>
+</template>
+
+<style lang="less" scoped>
+@import '../../../style/mixin.less';
+#head {
+  background: @blue;
+  position: fixed;
+  display: flex;
+  top: 0;
+  left: 0;
+  .wh(100%, 2rem);
+
+  .home {
+    flex: 1;
+    line-height: 2rem;
+    margin-left: 0.5rem;
+    .sizeColor(0.65rem, @white);
+  }
+
+  .login {
+    flex: 1;
+    text-align: right;
+    line-height: 2rem;
+    margin-right: 0.5rem;
+    .sizeColor(0.65rem, @white);
+  }
+}
+</style>
